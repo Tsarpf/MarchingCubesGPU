7e76f29e5f982076ee5062df5adc2681a0b9256c
Teemu Sarapisto <tsarpf@gmail.com>
2014-07-31 23:43:12 +0300

    testing three

--- GPUMarchingCubes/GPUMarchingCubes/DirectXApp.cpp
+++ GPUMarchingCubes/GPUMarchingCubes/DirectXApp.cpp
@@ -12,6 +12,39 @@ DirectXApp::~DirectXApp()
 
 bool DirectXApp::init()
 {
+    UINT32 AdapterOrdinal = 0;
+    D3DDEVTYPE DeviceType = D3DDEVTYPE_HAL;
+    D3DCAPS9 caps;
+    m_pD3D->GetDeviceCaps(AdapterOrdinal, DeviceType, &caps); // caps bits
+
+    D3DPRESENT_PARAMETERS params;
+    ZeroMemory(&params, sizeof(D3DPRESENT_PARAMETERS));
+
+    // Swap chain parameters:
+    params.hDeviceWindow = m_hWnd;
+    params.AutoDepthStencilFormat = D3DFMT_D24X8;
+    params.BackBufferFormat = D3DFMT_X8R8G8B8;
+    params.MultiSampleQuality = D3DMULTISAMPLE_NONE;
+    params.MultiSampleType = D3DMULTISAMPLE_NONE;
+    params.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    params.Windowed = true;
+    params.PresentationInterval = 0;
+    params.BackBufferCount = 2;
+    params.BackBufferWidth = 0;
+    params.BackBufferHeight = 0;
+    params.EnableAutoDepthStencil = true;
+    params.Flags = 2;
+
+    m_pD3D->CreateDevice(
+        0,
+        D3DDEVTYPE_HAL,
+        m_hWnd,
+        64,
+        &params,
+        &m_pd3dDevice
+        );
+
+
 }
 
 bool DirectXApp::run()
--- GPUMarchingCubes/GPUMarchingCubes/Main.cpp
+++ GPUMarchingCubes/GPUMarchingCubes/Main.cpp
@@ -1,7 +1,7 @@
+#include <windows.h>    // include the basic windows header file
 
-
-int main(int argc, char** argv)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-
+    MessageBox(NULL, L"Hello World!", L"Testing one two three!", MB_ICONEXCLAMATION | MB_OK);
     return 0;
 }
